8f50a40835098383f3dfec5e25c1bc365ee3f4f9
jstuckey <klash64@gmail.com>
2014-03-01 15:28:09 -0500

    Added models, controllers, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 DataEngineering::Application.routes.draw do
+  root to: "home#index"
+  post "upload", to: "home#upload"
+
+  resources :customers
+  resources :merchants
+  resources :items
+  resources :purchases
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
