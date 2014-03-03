d6f7e5105750d70537a626e7e847874d0a690e38
jstuckey <klash64@gmail.com>
2014-03-02 19:18:15 -0500

    Removed relationship.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,3 @@
 class Customer < ActiveRecord::Base
 	has_many :purchases
-	has_many :items, through :purchases
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,3 @@
 class Merchant < ActiveRecord::Base
 	has_many :items
-	has_many :purcahses, through :items
 end
