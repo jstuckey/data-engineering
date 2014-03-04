168510e807600b978089e15043b8206036afedde
jstuckey <klash64@gmail.com>
2014-03-04 18:11:51 -0500

    Added return values. Added error handling.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,58 +1,78 @@
 module ApplicationHelper
 	def parse_uploaded_file(file_name)
 
+		record_count = 0
+		error_count = 0
+		purchase_ids = Array.new
+
 		File.open(file_name).each_with_index do |line, index|
 
-			# Skip the column header.
-			# The spec says we can always assume a header line.
-			next if index == 0
+			begin
+				# Skip the column header.
+				# The spec says we can always assume a header line.
+				next if index == 0
 
-			# Split the line delimited by tabs.
-			# The spec says the columns are always separated by tabs.
-			columns = line.split /\t/
+				# Split the line delimited by tabs.
+				# The spec says the columns are always separated by tabs.
+				columns = line.split /\t/
 
-			# Break each column into its own object.
-			# The spec says the columns are always in the same order and always present.
-			customer_name = columns[0]
-			item_description = columns[1]
-			item_price = columns[2]
-			purchase_count = columns[3]
-			merchant_address = columns[4]
-			merchant_name = columns[5]
+				# Break each column into its own object.
+				# The spec says the columns are always in the same order and always present.
+				customer_name = columns[0]
+				item_description = columns[1]
+				item_price = columns[2]
+				purchase_count = columns[3]
+				merchant_address = columns[4]
+				merchant_name = columns[5]
 
-			# Convert these raw variables into Active Record models
+				# Convert these raw variables into Active Record models
 
-			# Either fetch or create a customer based on his/her name
-			customer = Customer.find_or_create_by( name: customer_name )
+				# Either fetch or create a customer based on his/her name
+				customer = Customer.find_or_create_by( name: customer_name )
 
-			# Either fetch or create a merchant based on its name
-			merchant = Merchant.find_or_create_by( name: merchant_name ) do |new_merchant|
-				# This block is called if creating a new merchant
-				new_merchant.address = merchant_address
+				# Either fetch or create a merchant based on its name
+				merchant = Merchant.find_or_create_by( name: merchant_name ) do |new_merchant|
+					# This block is called if creating a new merchant
+					new_merchant.address = merchant_address
+				end
+
+				# Either fetch or create an item based on its description
+				item = Item.find_or_create_by( description: item_description ) do |new_item|
+					# This block is called if creating a new item
+					new_item.price = item_price
+					new_item.merchant = merchant
+				end
+
+				# Create a new purchase record
+				purchase = Purchase.new({ count: purchase_count })
+
+				# Add the item to the purchase
+				purchase.items << item
+
+				# Add the purchase to the user's collection
+				customer.purchases << purchase
+
+				# Save the models
+				customer.save
+				merchant.save
+				item.save
+				purchase.save
+
+				# Add to the array of purchase_ids
+				purchase_ids << purchase.id
+
+				# Up the record count so we can return it
+				record_count = record_count + 1
+
+			rescue => e
+
+				Rails.logger.error e
+
+				error_count = error_count + 1
 			end
-
-			# Either fetch or create an item based on its description
-			item = Item.find_or_create_by( description: item_description ) do |new_item|
-				# This block is called if creating a new item
-				new_item.price = item_price
-				new_item.merchant = merchant
-			end
-
-			# Create a new purchase record
-			purchase = Purchase.new({ count: purchase_count })
-
-			# Add the item to the purchase
-			purchase.items << item
-
-			# Add the purchase to the user's collection
-			customer.purchases << purchase
-
-			# Save the models
-			customer.save
-			merchant.save
-			item.save
-			purchase.save
 
 		end
+
+		return { record_count: record_count, error_count: error_count, purchase_ids: purchase_ids }
 	end
 end
